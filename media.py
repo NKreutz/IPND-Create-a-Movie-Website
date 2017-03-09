4dff62779dbf0328429b1c88efb4a3c852d64978
Narissa Kreutz <narissakreutz@gmail.com>
2017-03-09 16:43:34 -0700

    Define init function in media.py

--- media.py
+++ media.py
@@ -3,9 +3,8 @@ import webbrowser
 class Movie(object):
 
     def __init__(self, movie_title, poster_image, movie_trailer):
+        ''' Initializes/Creates space in memory for instances(self) of class "Movie"
+        for the instance's title, poster image and trailer url'''
         self.title = movie_title
         self.poster_image_url = poster_image
         self.trailer_youtube_url = movie_trailer
-
-    def show_trailer(self):
-        webbrowser.open(self.trailer_youtube_url)
