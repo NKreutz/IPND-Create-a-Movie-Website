3073814887247d5acdf459f4a2d8ae97981e75c6
Narissa Kreutz <narissakreutz@gmail.com>
2017-03-09 17:58:35 -0700

    Doc info filled out

--- media.py
+++ media.py
@@ -1,7 +1,8 @@
 import webbrowser
 
 class Movie(object):
-
+    """This class provides a way to store movie related information"""
+    
     def __init__(self, movie_title, poster_image, movie_trailer):
         ''' Initializes/Creates space in memory for instances(self) of class "Movie"
         for the instance's title, poster image and trailer url'''
